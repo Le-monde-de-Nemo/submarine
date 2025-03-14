ac0ae362c31508a99a42295bec7316ae7effd5de
Nemo D'ACREMONT <ndacremont@enseirb-matmeca.fr>
2025-03-14 21:47:51 +0100

    feat: make repl easier to customize by having a repl structure

--- src/repl/repl.h
+++ src/repl/repl.h
@@ -4,8 +4,29 @@
 
 #define REPL_BUF_SIZE 2048
 
+struct repl_entry {
+    // Returns non-zero if the entry match argv, zero otherwize
+    int (*match)(int argc, char* argv[]);
+    void (*exec)(int argc, char* argv[]);
+};
+
+struct repl {
+    struct repl_entry* repls;
+    size_t replc;
+};
+
 /*
- *  Run the repl lmao
+ *  Create an reple using the an array of repl_entries
  */
-int repl__run();
+struct repl repl__create(struct repl_entry* repl_entries, size_t n);
+
+/*
+ *  Run the repl
+ */
+int repl__run(struct repl repl);
+
+/*
+ *  Call to free the repl
+ */
+void repl__finalize(struct repl repl);
 #endif
