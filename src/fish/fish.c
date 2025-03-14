5bc735fa59beed6e9bc8b992afd367e1d6520ee7
damdam166 <dericoo166@gmail.com>
2025-03-14 23:06:21 +0100

    feat: fish header.

--- src/fish/fish.c
+++ src/fish/fish.c
@@ -1,42 +1,68 @@
 // --------------------------------------------------------------------------
 // The implementation of the FISH structure.
-// See `/src/fish/fish.h`.
+//      See `/src/fish/fish.h`.
+//      Dependencies:
+//              - `vec2.h` for the coordinates.
 // --------------------------------------------------------------------------
 
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "fish.h"
+#include "vec2.h"
 
 // --------------------------------------------------------------------------
 
-struct Fish* fish__init_fish()
+struct Fish fish__init_fish()
 {
-    return NULL;
+    struct Fish fish;
+    fish.specie = COMMON;
+    fish.coordinates = vec2__zeros();
+
+    return fish;
 }
 
 // --------------------------------------------------------------------------
 
 enum Species fish__getType(const struct Fish* fish)
 {
+    // TODO
     return COMMON;
 }
 
-enum Species fish__setType(struct Fish* fish)
+struct Fish* fish__setType(const enum Species specie, struct Fish* fish)
 {
-    return COMMON;
+    // TODO
+    return fish;
+}
+
+// --------------------------------------------------------------------------
+
+struct vec2  fish__getPos(const struct Fish* fish)
+{
+    // TODO
+    return vec2__zeros();
+}
+
+struct Fish* fish__setPos(const struct vec2 pos, struct Fish* fish)
+{
+    // TODO
+    return fish;
 }
 
 // --------------------------------------------------------------------------
 
 int fish__destroy_fish(struct Fish* ptr_fish)
 {
+    // TODO
     return 0;
 }
 
 // --------------------------------------------------------------------------
 
+#ifdef DEBUG_FISH
 int main(int argc, char** argv)
 {
     return 0;
 }
+#endif // DEBUG_FISH
--- src/fish/fish.h
+++ src/fish/fish.h
@@ -1,6 +1,8 @@
 #ifndef __FISH__H__
 #define __FISH__H__
 
+    #include "vec2.h"
+
     // ----------------------------------------------------------------------
     // The FISH structure!
     // This one contains 2 type of fish :
@@ -19,28 +21,41 @@
 
     struct Fish {
         enum Species specie;
-        // TODO
+        struct vec2 coordinates;
     };
 
     // ----------------------------------------------------------------------
     // Create a fish.
+    //      type = COMMON.
+    //      pos  = (0, 0).
+    //
+    //      The pos is actually a `vec2`. See `src/utils/vec2.h`.
     // ----------------------------------------------------------------------
 
-    // TODO
-    struct Fish* fish__init_fish();
+    struct Fish fish__init_fish();
 
     // ----------------------------------------------------------------------
-    // By default, the type of the fish is COMMON.
+    // By default, the type of the fish is COMMON, see `enum SPECIES`.
     // ----------------------------------------------------------------------
 
     enum Species fish__getType(const struct Fish* fish);
-    enum Species fish__setType(struct Fish* fish);
+    struct Fish* fish__setType(const enum Species specie, struct Fish* fish);
+
+    // ----------------------------------------------------------------------
+    // To access to the position of the fish.
+    //      By default, the fish is at (0, 0).
+    //      You can set the fish everywhere.
+    //          it does not verify if the coordinates are in the aquarium.
+    // ----------------------------------------------------------------------
+
+    struct vec2  fish__getPos(const struct Fish* fish);
+    struct Fish* fish__setPos(const struct vec2 pos, struct Fish* fish);
 
     // ----------------------------------------------------------------------
     // Remove the allocated area from a fish creation.
+    //      The fish is allocated on the stack.
     // ----------------------------------------------------------------------
 
-    // TODO
     int fish__destroy_fish(struct Fish* ptr_fish);
 
     // ----------------------------------------------------------------------
