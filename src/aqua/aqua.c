860c761cd38846d0132906f27fc9334caa613f45
damdam166 <dericoo166@gmail.com>
2025-03-18 10:48:58 +0100

    feat[aqua]: first version of the aquarium. Need to use an implementation of a list to store dinamically the fishes and the vues without having recode it.

---
+++ src/aqua/aqua.c
@@ -0,0 +1,115 @@
+// --------------------------------------------------------------------------
+// The implementation of the aqua(rium) structure.
+//      See `/src/aqua/aqua.h`.
+//      Dependencies:
+//              - `vec2.h` for the coordinates, and sizes.
+//              - `figure.h` for the id, the pos and the size of a fish/vue.
+//              - `fish.h` to manipulate a fish.
+//              - `vue.h` to manipulate a vue.
+//
+//      The structure contains:
+//          - a fig, which is the id, the coord and the size of the aqua.
+//              See `src/figure/figure.h`
+// --------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "aqua.h"
+#include "figure.h"
+#include "fish.h"
+#include "vec2.h"
+#include "vue.h"
+
+// --------------------------------------------------------------------------
+
+struct aqua_t aqua__init_aqua(struct vec2 size)
+{
+    struct aqua_t aqua = {
+        .fig = figure__init_figure(0, vec2__zeros(), size)
+    };
+
+    return aqua;
+}
+
+// --------------------------------------------------------------------------
+
+int aqua__get_id(const struct aqua_t* ptr_aqua)
+{
+    // TODO
+    return 0;
+}
+
+// --------------------------------------------------------------------------
+
+struct aqua_t
+aqua__add_fish(struct fish_t* ptr_fish, const struct aqua_t* ptr_aqua)
+{
+    // TODO
+    return *ptr_aqua;
+}
+
+struct aqua_t
+aqua__del_fish(int id_fish, const struct aqua_t* ptr_aqua)
+{
+    // TODO
+    return *ptr_aqua;
+}
+
+struct fish_t
+aqua__get_fish(int id_fish, const struct aqua_t* ptr_aqua)
+{
+    // TODO
+    struct fish_t fish = fish__init_fish(
+        COMMON,
+        0, vec2__zeros(), vec2__ones(),
+        NULL);
+
+    return fish;
+}
+
+struct fish_t*
+aqua__get_fishes(const struct aqua_t* ptr_aqua)
+{
+    // TODO
+    return NULL;
+}
+
+// --------------------------------------------------------------------------
+
+struct aqua_t
+aqua__add_vue(struct vue_t* ptr_vue, const struct aqua_t* ptr_aqua)
+{
+    return *ptr_aqua;
+}
+
+struct aqua_t
+aqua__del_vue(int id_vue, const struct aqua_t* ptr_aqua)
+{
+    return *ptr_aqua;
+}
+
+struct vue_t
+aqua__get_vue(int id_vue, const struct aqua_t* ptr_aqua)
+{
+    // TODO
+    struct vue_t vue = vue__init_vue(0, vec2__zeros(), vec2__ones());
+    return vue;
+}
+
+struct vue_t*
+aqua__get_vues(const struct aqua_t* ptr_aqua)
+{
+    // TODO
+    return NULL;
+}
+
+// --------------------------------------------------------------------------
+
+int aqua__destroy_aqua(struct aqua_t* ptr_aqua)
+{
+    // TODO
+    return 1;
+}
+
+// ----------------------------------------------------------------------
---
+++ src/aqua/aqua.h
@@ -0,0 +1,97 @@
+#ifndef __AQUA__H__
+#define __AQUA__H__
+
+#include "figure.h"
+#include "fish.h"
+#include "vec2.h"
+#include "vue.h"
+
+// ----------------------------------------------------------------------
+// The header of the aqua(rium).
+//      See `src/aqua/aqua.c`.
+//      Dependencies:
+//              - `vec2.h` for the coordinates.
+//              - `fish.h` for the fishes.
+//              - `vue.h`  for the views.
+// ----------------------------------------------------------------------
+
+/* It could be a good idea to put this struct in `src/fish/fish.c`.
+ * However, we want to allocate the fishes on the stack.
+ * If we put the struct in `src/fish/fish.c` we will need to use malloc.
+ */
+struct aqua_t {
+    struct figure_t fig; // id, size and coord of the aquarium.
+                         // See `src/figure/figure.h`
+
+    // int nb_vues;
+    // struct vue_t* array_vue;
+    //
+    // int nb_fishes;
+    // struct fish_t* array_fishes;
+};
+
+// ----------------------------------------------------------------------
+// Create an aqua(rium).
+//  By default:
+//      size       = (100, 100).
+// ----------------------------------------------------------------------
+
+struct aqua_t aqua__init_aqua(struct vec2 size);
+
+// ----------------------------------------------------------------------
+
+int aqua__get_id(const struct aqua_t* ptr_aqua);
+
+// ----------------------------------------------------------------------
+// To manipulate fishes in the aquarium.
+//      For now, there is no fish limit.
+//      You can set the fish everywhere.
+//      However, if it is not in the aquarium, the fish will not be set.
+//
+//              See `src/fish/fish.h` to individually manipulate a fish.
+// ----------------------------------------------------------------------
+
+struct aqua_t
+aqua__add_fish(struct fish_t* ptr_fish, const struct aqua_t* ptr_aqua);
+
+struct aqua_t
+aqua__del_fish(int id_fish, const struct aqua_t* ptr_aqua);
+
+struct fish_t
+aqua__get_fish(int id_fish, const struct aqua_t* ptr_aqua);
+
+struct fish_t*
+aqua__get_fishes(const struct aqua_t* ptr_aqua);
+
+// ----------------------------------------------------------------------
+// To manipulate vues in the aquarium.
+//      For now, there is no vue limit.
+//      You can set the vue everywhere.
+//      However, if it is not in the aquarium, the vue will not be set.
+//      If the vue is not fully in the aquarium, it will not be set.
+//
+//              See `src/vue/vue.h` to individually manipulate a vue.
+// ----------------------------------------------------------------------
+
+struct aqua_t
+aqua__add_vue(struct vue_t* ptr_vue, const struct aqua_t* ptr_aqua);
+
+struct aqua_t
+aqua__del_vue(int id_vue, const struct aqua_t* ptr_aqua);
+
+struct vue_t
+aqua__get_vue(int id_vue, const struct aqua_t* ptr_aqua);
+
+struct vue_t*
+aqua__get_vues(const struct aqua_t* ptr_aqua);
+
+// ----------------------------------------------------------------------
+// Remove the allocated area from a aqua(rium) creation.
+//      The aqua is allocated on the stack.
+//      It returns 0 if it has been done with error.
+// ----------------------------------------------------------------------
+
+int aqua__destroy_aqua(struct aqua_t* ptr_aqua);
+
+// ----------------------------------------------------------------------
+#endif // __AQUA__H__
