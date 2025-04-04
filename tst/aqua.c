893d3f837858cbdac6a83c36a2f3be2022e4cbdd
damdam166 <dericoo166@gmail.com>
2025-04-04 13:44:01 +0200

    feat(aqua): all tests are done.

--- tst/aqua.c
+++ tst/aqua.c
@@ -171,62 +171,62 @@ int test__aqua_add_vue(void)
             "aqua__get_nb_vues(init_aqua + i vue) should be i\n");
     }
 
-    //for (i = 0; i < 100; ++i) {
-        //aqua = aqua__del_vue(i + 1, aqua);
-//
-        //local_ret |= myassert(
-            //aqua__get_nb_vues(aqua) == 100 - i - 1,
-            //"aqua__get_nb_vues(init_aqua - i vue) should be i\n");
-    //}
+    for (i = 0; i < 100; ++i) {
+        aqua = aqua__del_vue(i + 1, aqua);
 
-    //for (i = 0; i < 100; ++i) {
-        //struct vue_t* vue = aqua__get_vue(i, aqua);
-        //local_ret |= myassert(
-            //vue == NULL,
-            //"aqua__get_vue(i vue, NULL) should be NULL after deletion\n");
-    //}
-//
-    //local_ret |= myassert(
-        //aqua__get_vues(aqua) == NULL,
-        //"aqua__get_vues(init_aqua) should be NULL\n");
-//
-    //for (i = 0; i < 100; ++i) {
-        //struct vue_t vue = vue__init_vue(i + 1, vec2__zeros(), vec2__ones());
-//
-        //aqua = aqua__add_vue(vue, aqua);
-    //}
-//
-    //struct vue_t* vue = aqua__get_vue(50, aqua);
-//
-    //local_ret |= myassert(
-        //vue != NULL,
-        //"aqua__get_vue(50, aqua) should not be NULL\n");
-//
-    //if (vue != NULL) {
-        //local_ret |= myassert(
-            //vue__get_id(vue) == 50,
-            //"vue_id(aqua__get_vue)(50, aqua) should not be 50\n");
-    //}
-//
-    //local_ret |= myassert(
-        //aqua__destroy_aqua(&aqua) == 1,
-        //"aqua__destroy_aqua(init_aqua) should be 1\n");
-//
-    //aqua = aqua__init_aqua(vec2__ones());
-    //for (i = 0; i < 100; ++i) {
-        //struct vue_t vue = vue__init_vue(i + 1, vec2__zeros(), vec2__ones());
-//
-        //aqua = aqua__add_vue(vue, aqua);
-    //}
-//
-    //struct vue_t* array_vues = aqua__get_vues(aqua);
-    //for (i = 0; i < 100; ++i) {
-        //int id = vue__get_id(&(array_vues[i]));
-        //local_ret |= myassert(
-            //id <= 100 && 0 < id,
-            //"aqua__get_id(aqua__get_vues(100 vues))(i) should be i<=100, 0<i\n");
-    //}
-    //free(array_vues);
+        local_ret |= myassert(
+            aqua__get_nb_vues(aqua) == 100 - i - 1,
+            "aqua__get_nb_vues(init_aqua - i vue) should be i\n");
+    }
+
+    for (i = 0; i < 100; ++i) {
+        struct vue_t* vue = aqua__get_vue(i, aqua);
+        local_ret |= myassert(
+            vue == NULL,
+            "aqua__get_vue(i vue, NULL) should be NULL after deletion\n");
+    }
+
+    local_ret |= myassert(
+        aqua__get_vues(aqua) == NULL,
+        "aqua__get_vues(init_aqua) should be NULL\n");
+
+    for (i = 0; i < 100; ++i) {
+        struct vue_t vue = vue__init_vue(i + 1, vec2__zeros(), vec2__ones());
+
+        aqua = aqua__add_vue(vue, aqua);
+    }
+
+    struct vue_t* vue = aqua__get_vue(50, aqua);
+
+    local_ret |= myassert(
+        vue != NULL,
+        "aqua__get_vue(50, aqua) should not be NULL\n");
+
+    if (vue != NULL) {
+        local_ret |= myassert(
+            vue__get_id(*vue) == 50,
+            "vue_id(aqua__get_vue)(50, aqua) should not be 50\n");
+    }
+
+    local_ret |= myassert(
+        aqua__destroy_aqua(&aqua) == 1,
+        "aqua__destroy_aqua(init_aqua) should be 1\n");
+
+    aqua = aqua__init_aqua(vec2__ones());
+    for (i = 0; i < 100; ++i) {
+        struct vue_t vue = vue__init_vue(i + 1, vec2__zeros(), vec2__ones());
+
+        aqua = aqua__add_vue(vue, aqua);
+    }
+
+    struct vue_t* array_vues = aqua__get_vues(aqua);
+    for (i = 0; i < 100; ++i) {
+        int id = vue__get_id(array_vues[i]);
+        local_ret |= myassert(
+            id <= 100 && 0 < id,
+            "aqua__get_id(aqua__get_vues(100 vues))(i) should be i<=100, 0<i\n");
+    }
+    free(array_vues);
 
     local_ret |= myassert(
         aqua__destroy_aqua(&aqua) == 1,
